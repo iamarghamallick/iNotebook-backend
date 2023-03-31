09ed00ef67950ee100afb48b9a5a6b4e00f96be6
Argha Mallick <arghamallick5@gmail.com>
2023-03-31 18:35:40 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const mongoURI = process.env.mongoURI
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo;
---
+++ index.js
@@ -0,0 +1,20 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors') 
+require('dotenv').config()
+
+connectToMongo();
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
+app.listen(PORT, () => {
+  console.log(`iNotebook backend listening at http://localhost:${PORT}`)
+})
